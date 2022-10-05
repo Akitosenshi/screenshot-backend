b6bab622ac4771c1c82257794faa1f2a1f3eed1a
Akito <mallueck@gmail.com>
2022-10-05 20:56:17 +0200

    file upload

--- src/api/file.rs
+++ src/api/file.rs
@@ -1,34 +1,27 @@
 use magic::MagicError;
-use rocket::{post, Data, data::{Limits, ToByteUnit}};
+use rocket::{post, data::{Data, Limits, ToByteUnit}, tokio::io::AsyncWriteExt};
 use uuid::Uuid;
+use rocket::tokio::fs::File;
 
 #[post("/file", data = "<data>")]
-pub async fn upload(mut data: Data<'_>, limits: &Limits) -> std::io::Result<String> {
+pub async fn upload(data: Data<'_>, limits: &Limits) -> std::io::Result<String> {
 	let name = Uuid::new_v4().to_string().replace('-', "");
-	let buf = data.peek(512).await;
-	let ext = if let Some(res) = infer::get(&buf) {
-		res.extension()
-	} else {
-		if let Ok(res) = get_type(buf) {
-			res
-		} else {
-			""
-		}
-	};
-	let path = format!("/tmp/screenshot/{name}.{ext}");
-	data.open(limits.get("file").unwrap_or(1.mebibytes())).into_file(path).await?;
+
+	let mut buffer: Vec<u8> = Vec::with_capacity(limits.get("file").unwrap_or(4.mebibytes()).as_u64() as usize);
+	data.open(limits.get("file").unwrap_or(4.mebibytes())).stream_to(&mut buffer).await?;
+
+	let ext = get_ext(&buffer).await.unwrap_or("".to_string());
+	let path = format!("{}/{}.{}", "/tmp/screenshot", name, ext);
+
+	File::create(path).await?.write_all(&buffer).await?;
 
 	Ok(format!("http://localhost:8000/files/{name}.{ext}"))
 }
 
-fn get_type(buf: &[u8]) -> Result<&str, MagicError> {
+async fn get_ext(buffer: &Vec<u8>) -> Result<String, MagicError> {
 	let cookie = magic::Cookie::open(magic::CookieFlags::EXTENSION | magic::CookieFlags::ERROR)?;
 	cookie.load::<&str>(&[])?;
-	let tmp = cookie.buffer(&buf)?;
-	let exts: Vec<&str> = tmp.split('/').filter(|ext| ext.len() <= 3).collect();
-	if let Some(res) = exts.first() {
-		Ok(res)
-	} else {
-		Ok("")
-	}
+	let tmp = cookie.buffer(&buffer)?;
+	let ext = tmp.split('/').filter(|ext| ext.len() <= 3).next().unwrap_or("");
+	Ok(ext.to_string())
 }
